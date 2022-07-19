40e2afe781a4b09eac0a182d2ef7c0dbb15777a2
jeffreylin0723 <qwertzxc1478963@gmail.com>
2022-07-19 18:55:55 +0800

    add form

--- packages/web/schema/ceramic/user.ts
+++ packages/web/schema/ceramic/user.ts
@@ -1,12 +1,15 @@
 export type BasicProfile = {
     name?: string;
-    birthday?: number;
-    gender?: number; //0 women 1 men 2 everyone
-    showMe?: string; //0 women 1 men 2 everyone 
+
+}
+export type UserProfile = {
+    name: string;
+    birthday: number;
+    gender: number; //0 women 1 men 2 everyone
+    showMe: number; //0 women 1 men 2 everyone 
     showMyGenderOnProfile?: boolean;
     importNFT?: boolean;
     addOnChainActivity?: boolean;
     organizations?: string[];
     passion?: string[];
-
 }
