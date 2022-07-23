941104266279290977f1dfade89b3f2967d4d063
GregShen <gregshen0925@gmail.com>
2022-07-24 01:35:51 +0800

    IMatching comment

--- packages/hardhat/contracts/ITind3rMatching.sol
+++ packages/hardhat/contracts/ITind3rMatching.sol
@@ -4,12 +4,16 @@ pragma solidity ^0.8.12;
 import "@openzeppelin/contracts-upgradeable/interfaces/IERC1155Upgradeable.sol";
 
 interface ITind3rMatching is IERC1155Upgradeable {
+    // Emit Match if both users like each other
     event Match(uint256 aUserId, uint256 bUserId);
 
+    // Emit Block when one address dislike another address
     event Block(uint256 aUserId, uint256 bUserId);
 
+    // Revert when user isn't Tind3r user
     error NotCallByMembershipContract();
 
+    // Mint A's token to B and B's to A if match
     function mint(
         address aUser,
         uint256 aUserId,
@@ -17,6 +21,7 @@ interface ITind3rMatching is IERC1155Upgradeable {
         uint256 bUserId
     ) external;
 
+    // Get user all matches
     function getMatches(address user)
         external
         view
