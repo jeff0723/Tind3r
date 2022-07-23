4342aea7e293af18756179206761413a3fff04c5
GregShen <gregshen0925@gmail.com>
2022-07-23 09:17:22 +0800

    modify ITinderMembership.sol

--- packages/hardhat/contracts/ITind3rMembership.sol
+++ packages/hardhat/contracts/ITind3rMembership.sol
@@ -12,5 +12,31 @@ interface ITind3rMembership is IERC721AUpgradeable {
 
     function createProfile(Tind3rProfile calldata userProfile) external;
 
+    function updateProfile(Tind3rProfile calldata userProfile) external;
+
+    function like(address user) external;
+
     function getUserId(address user) external view returns (uint256 userId);
+
+    function isTind3rMember(address user) external view returns (bool);
+
+    function tokenURI(uint256 tokenId) external view returns (string memory);
+
+    function ifLike(address userA, address userB) external view returns (bool);
+
+    function prefixURI() external view returns (string memory);
+
+    function metadataUIR() external view returns (string memory);
+
+    function userStreamIdList(uint256 startId, uint256 endId)
+        external
+        view
+        returns (string memory);
+
+    function isMatched(address userA, address userB)
+        external
+        view
+        returns (bool);
+
+    function getMatches(address user) external view returns (uint32[] memory);
 }
